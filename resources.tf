828489fb30b91abcd2c798ec8310b58e6d962048
Yevgeni Shapiro <yevgeni.shapiro@gmail.com>
2024-12-31 22:36:43 +0000

    update

--- resources.tf
+++ resources.tf
@@ -1,4 +1,86 @@
 
+data "external" "subnet" {
+  program = ["/bin/bash", "-c", "docker network inspect -f '{{json .IPAM.Config}}' kind | jq .[0]"]
+  depends_on = [
+    kind_cluster.default
+  ]
+}
+resource "helm_release" "metallb" {
+  name             = "metallb"
+  repository       = "https://metallb.github.io/metallb"
+  chart            = "metallb"
+  namespace        = "metallb"
+  version          = "0.10.3"
+  create_namespace = true
+  timeout = 300
+  values = [
+    <<-EOF
+  configInline:
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      # - ${cidrhost(data.external.subnet.result.Subnet, 150)}-${cidrhost(data.external.subnet.result.Subnet, 200)}
+      - 172.18.255.1-172.18.255.250
+  EOF
+  ]
+  depends_on = [
+    kind_cluster.default
+  ]
+}
+
+resource "null_resource" "wait_for_metallb" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the metallb controller...\n"
+      kubectl wait --namespace ${helm_release.metallb.namespace} \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=speaker \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [helm_release.metallb]
+}
+
+
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = var.ingress_nginx_helm_version
+
+  namespace        = var.ingress_nginx_namespace
+  create_namespace = true
+  timeout = 300
+
+  values = [file("config/nginx_values.yaml")]
+
+  depends_on = [kind_cluster.default]
+}
+
+resource "null_resource" "wait_for_ingress_nginx" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the nginx ingress controller...\n"
+      kubectl wait --namespace ${helm_release.ingress_nginx.namespace} \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=controller \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [helm_release.ingress_nginx]
+}
+
 resource "helm_release" "kargo" {
   namespace  = "kargo"
   create_namespace = true
